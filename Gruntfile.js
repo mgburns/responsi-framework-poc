58691f67b31e79c77fbd0318f559912f8818e87c
Mike Burns <mgburns@bu.edu>
2014-04-11 17:14:56 -0400

    Don't run jshint for uglified scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function( grunt ) {
 			options: {
 				jshintrc: '.jshintrc'
 			},
-			theme: [ 'js/**/*.js' ],
+			theme: [ 'js/src/*.js' ],
 		},
 
 		// Javascript minification
